9e412db83a002a1699f06043b952d130fdd36bd9
Muyale Okoyo <annmuyale@gmail.com>
2023-06-01 11:33:45 +0300

    first commit

--- app/src/main/java/com/example/contactlist/MainActivity.kt
+++ app/src/main/java/com/example/contactlist/MainActivity.kt
@@ -27,9 +27,17 @@ class MainActivity : AppCompatActivity() {
         var contacts5=ContactData("Kate","Katen","07869078","kate@gmail.com")
         var contacts6=ContactData("Paisley","Paisley","07869078","paisley@gmail.com")
         var contacts7=ContactData("Sera","Sera","07869078","sera@gmail.com")
+        var contacts8=ContactData("Sharon","Sharon","07869078","sharon@gmail.com")
+        var contacts9=ContactData("Charles","Charles","07869078","charlesmail.com")
+        var contacts10=ContactData("Sophie","Sophie","07869078","sophie@gmail.com")
+        var contacts11=ContactData("Ann","Ann","07869078","ann@gmail.com")
+        var contacts12=ContactData("Kate","Katen","07869078","kate@gmail.com")
+        var contacts13=ContactData("Paisley","Paisley","07869078","paisley@gmail.com")
+        var contacts14=ContactData("Sera","Sera","07869078","sera@gmail.com")
 
 
-    var contactList= listOf<ContactData>(contacts1,contacts2,contacts3,contacts4,contacts5,contacts6,contacts7)
+
+        var contactList= listOf<ContactData>(contacts1,contacts2,contacts3,contacts4,contacts5,contacts6,contacts7,contacts8,contacts9,contacts10,contacts11,contacts12,contacts13,contacts14)
         binding.rvcontacts.layoutManager=LinearLayoutManager(this)
         var contactadapter=ContactRvAdapter(contactList)
         binding.rvcontacts.adapter=contactadapter
